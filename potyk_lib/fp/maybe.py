bdece276a4222d6ac23be6988f8d9516e8d3fd82
potykion <potykion@gmail.com>
2023-01-10 13:58:01 +0300

    :sparkles: nullish & falsy

--- potyk_lib/fp/maybe.py
+++ potyk_lib/fp/maybe.py
@@ -7,6 +7,52 @@ __all__ = ['Maybe', 'Some', 'Nothing']
 
 
 class Maybe(Generic[T]):
+    def map(self, callable_):
+        """
+        >>> Some(123).map(lambda a: a+1).val
+        124
+        >>> Nothing().map(lambda a: a+1).val is None
+        True
+        """
+        if isinstance(self, Some):
+            return Some(callable_(self.val))
+        else:
+            return self
+
+    def __getattr__(self, item):
+        """
+        >>> falsy(None).attr is None
+        True
+        >>> list(falsy({'a': 1}).keys())
+        ['a']
+        """
+        if isinstance(self, Some):
+            return getattr(self.val, item)
+        else:
+            return None
+
+    @classmethod
+    def none_guess(cls, val):
+        """
+        >>> isinstance(Maybe.none_guess(None), Nothing)
+        True
+        >>> isinstance(Maybe.none_guess(''), Some)
+        True
+        """
+
+        return Nothing() if val is None else Some(val)
+
+    @classmethod
+    def falsy_guess(cls, val):
+        # type: (T) -> Maybe[T]
+        """
+        >>> isinstance(Maybe.falsy_guess(''), Nothing)
+        True
+        >>> isinstance(Maybe.falsy_guess(1), Some)
+        True
+        """
+        return Nothing() if not val else Some(val)
+
     def or_else(self, maybe_callable: Callable[[], 'Maybe']):
         if isinstance(self, Some):
             return self
@@ -24,6 +70,10 @@ class Maybe(Generic[T]):
             return self.val
         else:
             raise ValueError('Нельзя вызвать unwrap для Nothing')
+
+
+nullish = Maybe.none_guess
+falsy = Maybe.falsy_guess
 
 
 @dataclasses.dataclass(frozen=True)
